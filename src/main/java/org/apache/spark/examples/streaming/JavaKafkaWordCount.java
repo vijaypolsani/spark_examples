816d01a826fcbe3a9c0ba91d8f96864b2b2f0f5d
vijay.polsani <vijay.polsani@gmail.com>
2015-08-13 19:19:40 -0700

    Initial Test

---
+++ src/main/java/org/apache/spark/examples/streaming/JavaKafkaWordCount.java
@@ -0,0 +1,126 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.examples.streaming;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import scala.Tuple2;
+
+//import org.apache.spark.examples.streaming.StreamingExamples;
+
+/**
+ * Consumes messages from one or more topics in Kafka and does wordcount.
+ * <p>
+ * Usage: JavaKafkaWordCount <zkQuorum> <group> <topics> <numThreads>
+ * <zkQuorum> is a list of one or more zookeeper servers that make quorum
+ * <group> is the name of kafka consumer group
+ * <topics> is a list of one or more kafka topics to consume from
+ * <numThreads> is the number of threads the kafka consumer should use
+ * <p>
+ * To run this example:
+ * `$ bin/run-example org.apache.spark.examples.streaming.JavaKafkaWordCount zoo01,zoo02, \
+ * zoo03 my-consumer-group topic1,topic2 1`
+ */
+
+public final class JavaKafkaWordCount {
+	private static final Logger log = LoggerFactory.getLogger(JavaKafkaWordCount.class);
+
+	private static final String zkQuorum = "sjc1pzoo06.sj.lithium.com:2181," +
+			"sjc1pzoo07.sj.lithium.com:2181," +
+			"sjc1pzoo08.sj.lithium.com:2181," +
+			"sjc1pzoo09.sj.lithium.com:2181," +
+			"sjc1pzoo10.sj.lithium.com:2181/kafka/sjc1-kafka-1a";
+	private static final HashMap<String, Integer> topicMap = Maps.newHashMap();
+	private static final String groupId = "sparkStreming";
+	private static final int numThreads = 1;
+
+	static {
+		topicMap.put("all", numThreads);
+	}
+
+
+	private static final Pattern NEW_LINE = Pattern.compile("\r\n");
+
+	private JavaKafkaWordCount() {
+	}
+
+	//@Slf4J
+	public static void main(String[] args) {
+		//System.err.println("Usage: JavaKafkaWordCount <zkQuorum> <group> <topics> <numThreads>");
+
+		SparkConf sparkConf = new SparkConf().setMaster("local[1]").setAppName("JavaKafkaWordCount");
+		// Create the context with a 1 second batch size
+		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
+
+		JavaPairReceiverInputDStream<String, String> messages =
+				KafkaUtils.createStream(jssc, zkQuorum, groupId, topicMap);
+
+		JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
+			@Override
+			public String call(Tuple2<String, String> tuple2) {
+				return tuple2._2();
+			}
+		});
+		log.info("Number of lines: " + lines.count());
+
+		JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+			@Override
+			public Iterable<String> call(String x) {
+				return Lists.newArrayList(NEW_LINE.split(x));
+			}
+		});
+		log.info("Number of words: " + words.count());
+
+		JavaPairDStream<String, Integer> wordCounts = words.mapToPair(
+				new PairFunction<String, String, Integer>() {
+					@Override
+					public Tuple2<String, Integer> call(String s) {
+						return new Tuple2<String, Integer>(s, 1);
+					}
+				}).reduceByKey(new Function2<Integer, Integer, Integer>() {
+			@Override
+			public Integer call(Integer i1, Integer i2) {
+				return i1 + i2;
+			}
+		});
+
+		log.info("Number of Words & its Counts: " + wordCounts.count().count());
+
+		wordCounts.print();
+		jssc.start();
+		jssc.awaitTermination();
+	}
+}
